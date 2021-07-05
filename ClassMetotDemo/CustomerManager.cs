43e124f401b82be18f0fc03a2b658d44621d6cd3
ÇAĞRI TÜRKMEN <ARI TRKMEN@LAPTOP-DTGLMNBR>
2021-07-05 18:35:38 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId+": "+customer.CustomerName+"   "+ customer.CustomerSurname+ "        Added to this webpage...");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Registered Customers ----:    "+customer.CustomerId + "-" + customer.CustomerName + "   " + customer.CustomerSurname );
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + ": " + customer.CustomerName + "   " + customer.CustomerSurname + "         Deleted from this webpage...");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "Ahmet";
+            customer1.CustomerSurname = "Türkmen";
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Ayşe";
+            customer2.CustomerSurname = "Türkmen";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 3;
+            customer3.CustomerName = "Çağrı";
+            customer3.CustomerSurname = "Türkmen";
+
+            
+            
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine("....................Ekleme İşlemi................");
+
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            Console.WriteLine("----------------------Listeleme İşlemi ----------");
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            foreach (Customer customer in customers)
+            {
+                customerManager.List(customer);
+            }
+
+            Console.WriteLine("***********Silme İşlemi**********");
+
+
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+            customerManager.Delete(customer3);
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
